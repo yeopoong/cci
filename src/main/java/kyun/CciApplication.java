b5b29107ed9f9ab03bca27c6a141c58e82d01394
yeopoong <yeopoong@gmail.com>
2017-01-14 07:48:43 +0900

    first commit

---
+++ src/main/java/kyun/CciApplication.java
@@ -0,0 +1,12 @@
+package kyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CciApplication.class, args);
+	}
+}
